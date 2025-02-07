e70a09ba7ea74c0dab41dbb79a7259da0e0d75ad
notaneeeeeesh <anishomkar12@gmail.com>
2025-02-07 18:11:28 +0400

    finally fixed fixtures

--- attend/api/roles.py
+++ attend/api/roles.py
@@ -16,26 +16,27 @@ def get_user_role():
     
 def create_user_role_info(user, method):
     # Get assigned roles for the user
-    #roles = frappe.get_all("Has Role", filters={"parent": user.name}, fields=["role"])
-    role = frappe.get_all("Has Role", filters={"parent": user.name}, fields=["role"])[0]["role"]
-    # Check if user has one of the custom roles
-    if role in ["Attend Student", "Attend Faculty", "Attend Coordinator"]:
-        # Avoid duplicate entries
-        if not frappe.db.exists("Attend User", {"user": user.name}):
-            new_entry = frappe.get_doc({
-                "doctype": "Attend User",
-                "profile": user.name,
-                "role": role.removeprefix('Attend ')  # Store the assigned role
-            })
-            new_entry.insert()
-        if not frappe.db.exists(role, {"user": user.name}):
-            role_entry = frappe.get_doc({
-                "doctype": role,
-                "user": user.name
-            })
-            role_entry.insert()
-    frappe.db.commit()    
-            
+    roles = frappe.get_all("Has Role", filters={"parent": user.name}, fields=["role"])
+    if(len(roles) > 0 ):
+        role = roles[0]["role"]
+        # Check if user has one of the custom roles
+        if role in ["Attend Student", "Attend Faculty", "Attend Coordinator"]:
+            # Avoid duplicate entries
+            if not frappe.db.exists("Attend User", {"user": user.name}):
+                new_entry = frappe.get_doc({
+                    "doctype": "Attend User",
+                    "profile": user.name,
+                    "role": role.removeprefix('Attend ')  # Store the assigned role
+                })
+                new_entry.insert()
+            if not frappe.db.exists(role, {"user": user.name}):
+                role_entry = frappe.get_doc({
+                    "doctype": role,
+                    "user": user.name
+                })
+                role_entry.insert()
+        frappe.db.commit()    
+    return        
 
 # def create_attend_users_after_migrate():
 #     existing_users = frappe.get_all("User", fields=["name"])
